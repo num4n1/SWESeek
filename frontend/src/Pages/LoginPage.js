f5a60ac91c84914e38b1e7fa513a1d36d35a3da6
nickknapton12 <nickknapton12@gmail.com>
2021-11-02 15:29:17 -0600

    Added foundation for post requests

--- frontend/src/Pages/LoginPage.js
+++ frontend/src/Pages/LoginPage.js
@@ -1,23 +1,44 @@
-import { Form, Button, FloatingLabel, Container, Row, Col } from "react-bootstrap";
+import { Form, Button, FloatingLabel, Container } from "react-bootstrap";
 import "../Styles/LoginAndSignupStyles.css";
+import Axios from 'axios';
 
 export default function LoginPage() {
-  return (
-    <div style={{minHeight:`73.3vh`, paddingTop:`8%`}}>
-        <Container  style={{maxWidth:`900px`}}>
-        <h1 className="SignupHeader">Login</h1>
-        <Form className="SignupForm">
-            <FloatingLabel controlId="floatingInput" label="Email address" className="mb-3">
-                <Form.Control type="email" placeholder="name@sweseek.com" />
-            </FloatingLabel>
-            <FloatingLabel controlId="floatingInput" label="Password" className="mb-3">
-                <Form.Control type="password" placeholder="password" />
-            </FloatingLabel>
-            <Button size="lg" className="SignupButton" type="submit">
-                Submit
-            </Button>
-        </Form>
-    </Container>
-</div>
-  );
+  
+    function login(){
+        Axios.post("", {
+            "email": document.getElementById("email").value,
+            "password": document.getElementById("password").value,
+        })
+        .then(res => {
+            // Cookie setting
+        })
+        .catch(res => {
+            // Error Message
+        })
+    }
+
+    function keyPress(e){
+        if(e.key === "Enter"){
+            login();
+        }
+    }
+
+    return (
+        <div style={{minHeight:`73.3vh`, paddingTop:`8%`}}>
+            <Container  style={{maxWidth:`800px`}}>
+                <h1 className="SignupHeader">Login</h1>
+                <Form className="SignupForm" onSubmit={console.log("test")}>
+                    <FloatingLabel controlId="floatingInput" label="Email address" className="mb-3">
+                        <Form.Control id="email" type="email" placeholder="name@sweseek.com" />
+                    </FloatingLabel>
+                    <FloatingLabel controlId="floatingInput" label="Password" className="mb-3">
+                        <Form.Control id="password" onKeyPress={ keyPress } type="password" placeholder="password" />
+                    </FloatingLabel>
+                    <Button size="lg" className="SignupButton" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </Container>
+        </div>
+    );
 }
--- frontend/src/Pages/SignupPage.js
+++ frontend/src/Pages/SignupPage.js
@@ -1,50 +1,184 @@
-import { Form, Button, FloatingLabel, Container, Row, Col } from "react-bootstrap";
+import {
+  Form,
+  Button,
+  FloatingLabel,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
+import { useState } from "react";
 import "../Styles/LoginAndSignupStyles.css";
+import Axios from "axios";
 
-export default function SignupPage(){
-    return(
-        <div style={{minHeight:`73.3vh`}}>
-            <Container  style={{maxWidth:`900px`, paddingTop:`4%`}}>
-                <h1 className="SignupHeader">Sign up for free.</h1>
-                <Form className="SignupForm">
-                    <FloatingLabel controlId="floatingInput" label="Email address" className="mb-3">
-                        <Form.Control type="email" placeholder="name@sweseek.com" />
-                    </FloatingLabel>
-                    <Row>
-                        <Col>
-                            <FloatingLabel controlId="floatingInput" label="First Name" className="mb-3">
-                                <Form.Control type="text" placeholder="Dirst Name" />
-                            </FloatingLabel>
-                        </Col>
-                        <Col>
-                            <FloatingLabel controlId="floatingInput" label="Last Name" className="mb-3">
-                                <Form.Control type="text" placeholder="Last Name" />
-                            </FloatingLabel>
-                        </Col>
-                    </Row>
-                    <FloatingLabel controlId="floatingInput" label="Phone Number" className="mb-3">
-                            <Form.Control type="tel" placeholder="444-444-4444" pattern="[0-9]{3} [0-9]{3} [0-9]{4}" maxlength="12" />
-                    </FloatingLabel>
-                    <FloatingLabel controlId="floatingInput" label="Username" className="mb-3">
-                            <Form.Control type="text" placeholder="username" />
-                    </FloatingLabel>
-                    <Row>
-                        <Col>
-                            <FloatingLabel controlId="floatingInput" label="Password" className="mb-3">
-                                <Form.Control type="password" placeholder="password" />
-                            </FloatingLabel>
-                        </Col>
-                        <Col>
-                            <FloatingLabel controlId="floatingInput" label="Confirm Password" className="mb-3">
-                                <Form.Control type="password" placeholder="password" />
-                            </FloatingLabel>
-                        </Col>
-                    </Row>
-                    <Button size="lg" className="SignupButton" type="submit">
-                        Submit
-                    </Button>
-                </Form>
-            </Container>
-        </div>
-    )
+export default function SignupPage() {
+    const [emailValidity, setEmailValidity] = useState(false);
+    const [firstNameValidity, setFirstNameValidity] = useState(false);
+    const [lastNameValidity, setLastNameValidity] = useState(false);
+    const [phoneValidity, setPhoneValidity] = useState(false);
+    const [usernameValidity, setUsernameValidity] = useState(false);
+    const [passwordValidity, setPasswordValidity] = useState(false);
+    const [confirmPasswordValidity, setConfirmPasswordValidity] = useState(false);
+
+  function signup() {
+    if (
+      validateFields()
+    ) {
+      Axios.post("", {
+        email: document.getElementById("email").value,
+        firstName: document.getElementById("firstName").value,
+        lastName: document.getElementById("lastName").value,
+        phone: document.getElementById("phone").value,
+        userName: document.getElementById("userName").value,
+        password: document.getElementById("password").value,
+      })
+        .then((res) => {
+          // Cookie setting
+        })
+        .catch((res) => {
+          // Error Message
+        });
+    } else{
+        //Passwords do not match
+    }
+  }
+
+  function validateFields(){
+    let returnVal = true;
+    if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(document.getElementById("email").value)){
+        returnVal = false;
+        setEmailValidity(true);
+    }
+    if(document.getElementById("firstName").value.length < 2){
+        returnVal = false;
+        setFirstNameValidity(true);
+    }
+    if(document.getElementById("lastName").value.length < 2){
+        returnVal = false;
+        setLastNameValidity(true);
+    }
+    if(!/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.test(document.getElementById("phone").value)){
+        returnVal = false;
+        setPhoneValidity(true);
+    }
+    if(document.getElementById("username").value.length < 3){
+        returnVal = false;
+        setUsernameValidity(true);
+    }
+    if(document.getElementById("password").value !== document.getElementById("confirmPassword")){
+        returnVal = false;
+        setConfirmPasswordValidity(true);
+    }
+    if(document.getElementById("password").value.length < 6){
+        returnVal = false;
+        setPasswordValidity(true);
+    }
+    return returnVal;
+  }
+
+  return (
+    <div style={{ minHeight: `73.3vh` }}>
+      <Container style={{ maxWidth: `800px`, paddingTop: `4%` }}>
+        <h1 className="SignupHeader">Sign up for free.</h1>
+        <Form className="SignupForm" noValidate>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Email address"
+            className="mb-3"
+          >
+            <Form.Control
+              id="email"
+              type="email"
+              placeholder="name@sweseek.com"
+              required
+              isInvalid={emailValidity}
+            />
+          </FloatingLabel>
+          <Row>
+            <Col>
+              <FloatingLabel
+                controlId="floatingInput"
+                label="First Name"
+                className="mb-3"
+              >
+                <Form.Control
+                  id="firstName"
+                  type="text"
+                  placeholder="First Name"
+                  isInvalid={firstNameValidity}
+                />
+              </FloatingLabel>
+            </Col>
+            <Col>
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Last Name"
+                className="mb-3"
+              >
+                <Form.Control
+                  id="lastName"
+                  type="text"
+                  placeholder="Last Name"
+                  isInvalid={lastNameValidity}
+                />
+              </FloatingLabel>
+            </Col>
+          </Row>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Phone Number"
+            className="mb-3"
+          >
+            <Form.Control
+              id="phone"
+              type="tel"
+              placeholder="444-444-4444"
+              pattern="[0-9]{3} [0-9]{3} [0-9]{4}"
+              maxlength="12"
+              isInvalid={phoneValidity}
+            />
+          </FloatingLabel>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Username"
+            className="mb-3"
+          >
+            <Form.Control id="username" type="text" placeholder="username" isInvalid={usernameValidity} />
+          </FloatingLabel>
+          <Row>
+            <Col>
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Password"
+                className="mb-3"
+              >
+                <Form.Control
+                  id="password"
+                  type="password"
+                  placeholder="password"
+                  isInvalid={passwordValidity}
+                />
+              </FloatingLabel>
+            </Col>
+            <Col>
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Confirm Password"
+                className="mb-3"
+              >
+                <Form.Control
+                  id="confirmPassword"
+                  type="password"
+                  placeholder="password"
+                  isInvalid={confirmPasswordValidity}
+                />
+              </FloatingLabel>
+            </Col>
+          </Row>
+          <Button size="lg" className="SignupButton" onClick={signup}>
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    </div>
+  );
 }
