51f106dbdf44048a06d9ce70e64a2910777de0a3
Nick <nickknapton12@gmail.com>
2021-11-02 22:58:10 -0600

    work

--- frontend/src/Pages/TrackingPage.js
+++ frontend/src/Pages/TrackingPage.js
@@ -1,54 +1,159 @@
-import { Col, Container, Dropdown, Row, ButtonGroup } from 'react-bootstrap';
-import { useState, useEffect } from 'react';
-import '../Styles/TrackingPageStyles.css';
-import List from '../Components/List';
+import { Col, Container, Dropdown, Row, ButtonGroup } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import "../Styles/TrackingPageStyles.css";
+import List from "../Components/List";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
-export default function TrackingPage(){
-    const [lists, setLists] = useState([{}]);
-    const [currentList, setCurrentList] = useState({});
+const finalSpaceCharacters = [
+  {
+    id: "gary",
+    name: "Gary Goodspeed",
+    thumb: "./Assets/leef.png",
+  },
+  {
+    id: "steve",
+    name: "Steve Goodspeed",
+    thumb: "./Assets/leef.png",
+  },
+  {
+    id: "Tim",
+    name: "Tim Goodspeed",
+    thumb: "./Assets/leef.png",
+  },
+];
 
-    useEffect(() => {
-        generateMockData();
-    }, [])
+export default function TrackingPage() {
+  const [lists, setLists] = useState([{}]);
+  const [currentList, setCurrentList] = useState(lists[0]);
+  const [characters, setCharacters] = useState(finalSpaceCharacters);
 
-    function generateMockData(){
-        setLists([{name:"2019 Hunt", jobs:[{company:"Google"}, {company: "Facebook"}]},
-                 {name:"2020 Hunt", jobs:[{company:""}, {company: ""}]}, 
-                 {name:"2021 Hunt", jobs:[{company:""}, {company: ""}]}]);
-        setCurrentList(lists[0]);
-    }
+  useEffect(() => {
+    generateMockData();
+  }, []);
 
-    function changeList(list){
-        setCurrentList(list);
-    }
+  function generateMockData() {
+    setLists([
+      {
+        name: "2019 Hunt",
+        jobs: [{ company: "Google" }, { company: "Facebook" }],
+      },
+      { name: "2020 Hunt", jobs: [{ company: "" }, { company: "" }] },
+      { name: "2021 Hunt", jobs: [{ company: "" }, { company: "" }] },
+    ]);
+  }
 
+  function changeList(list) {
+    setCurrentList(list);
+  }
 
-    return(
-        <Container style={{minHeight:`73vh`}}>
-            <Row style={{marginTop:`2%`}}>
-                <Col style={{alignSelf:`center`}}>
-                    <Dropdown as={ButtonGroup}>
-                    <Dropdown.Toggle size="lg" className="ListSelectButton" >Job List</Dropdown.Toggle>
-                    <Dropdown.Menu>
-                    {
-                        lists.map(element => {
-                           return <Dropdown.Item onClick={() => {changeList(element)}}>{element.name}</Dropdown.Item>
-                        })
-                    }
-                    </Dropdown.Menu>
-                    </Dropdown>{' '}
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+    const items = Array.from(characters);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setCharacters(items);
+  }
+
+  return (
+    <Container style={{ minHeight: `73vh` }}>
+      <Row style={{ marginTop: `2%` }}>
+        <Col style={{ alignSelf: `center` }}>
+          <Dropdown as={ButtonGroup}>
+            <Dropdown.Toggle size="lg" className="ListSelectButton">
+              Job List
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {lists.map((element) => {
+                return (
+                  <Dropdown.Item
+                    onClick={() => {
+                      changeList(element);
+                    }}
+                  >
+                    {element.name}
+                  </Dropdown.Item>
+                );
+              })}
+            </Dropdown.Menu>
+          </Dropdown>{" "}
+        </Col>
+        <Col>
+          <h1 className="TrackingHeader">{currentList.name}</h1>
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="characters">
+            {(provided) => (
+              <div>
+                  <Row>
+                <Col>
+                <ul
+                  className="characters"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {characters.map(({ id, name, thumb }, index) => {
+                    return (
+                      <Draggable key={id} draggableId={id} index={index}>
+                        {(provided) => (
+                          <li
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            <div className="characters-thumb">
+                              <img src={thumb} alt={`${name} Thumb`} />
+                            </div>
+                            <p>{name}</p>
+                          </li>
+                        )}
+                      </Draggable>
+                    );
+                  })}
+                  {provided.placeholder}
+                </ul>
                 </Col>
                 <Col>
-                    <h1 className="TrackingHeader">{currentList.name}</h1>
+                <ul
+                  className="characters"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {characters.map(({ id, name, thumb }, index) => {
+                    return (
+                      <Draggable key={id} draggableId={id} index={index}>
+                        {(provided) => (
+                          <li
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            <div className="characters-thumb">
+                              <img src={thumb} alt={`${name} Thumb`} />
+                            </div>
+                            <p>{name}</p>
+                          </li>
+                        )}
+                      </Draggable>
+                    );
+                  })}
+                  {provided.placeholder}
+                </ul>
                 </Col>
-                <Col>
-                </Col>
-            </Row>
-            <Row>
-                <List list={currentList} />
-            </Row>
-        </Container>
-    )
+                </Row>
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+        {/*
+                    <List list={currentList} />
+                    */}
+      </Row>
+    </Container>
+  );
 }
 /*
                         <Col>
