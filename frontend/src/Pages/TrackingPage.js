a013c5f3b3a2e2920bc117a457c780e09f6bb529
nickknapton12 <nickknapton12@gmail.com>
2021-11-04 17:07:10 -0600

    Too tired

--- frontend/src/Pages/TrackingPage.js
+++ frontend/src/Pages/TrackingPage.js
@@ -6,11 +6,13 @@ import {
   ButtonGroup,
   Card,
   Modal,
+  Form,
+  Button,
 } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import "../Styles/TrackingPageStyles.css";
-import List from "../Components/List";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+const uuidv4 = require("uuid/v4");
 
 const exampleWishList = [
   {
@@ -127,8 +129,16 @@ export default function TrackingPage() {
   const [interviewItems, setInterviewItems] = useState(exampleInterview);
   const [offerItems, setOfferItems] = useState(exampleOffer);
   const [regectedItems, setRegectedItems] = useState(exampleRegected);
+  const [addJobColumn, setAddJobColumn] = useState();
+
+  const [addCompanyValid, setCompanyValid] = useState(false);
+  const [addPositionValid, setPositionValid] = useState(false);
+  const [addStartDateValid, setStartDateValid] = useState(false);
+  const [addLinkValid, setLinkValid] = useState(false);
+  const [addDescriptionValid, setDescriptionValid] = useState(false);
 
   const [show, setShow] = useState(false);
+  const [showAdd, setShowAdd] = useState(false);
   const [modalInfo, setModalInfo] = useState({});
 
   const handleClose = () => setShow(false);
@@ -136,6 +146,8 @@ export default function TrackingPage() {
     setModalInfo(item);
     setShow(true);
   };
+
+  const handleCloseAdd = () => setShowAdd(false);
 
   const [columns, setColumns] = useState({
     1: {
@@ -179,6 +191,7 @@ export default function TrackingPage() {
       const sourceItems = [...sourceColumn.items];
       const destItems = [...destColumn.items];
       const [removed] = sourceItems.splice(source.index, 1);
+      removed.applicationStatus = destColumn.name;
       destItems.splice(destination.index, 0, removed);
       setColumns({
         ...columns,
@@ -206,6 +219,66 @@ export default function TrackingPage() {
         },
       });
     }
+  }
+
+  function addJob() {
+    const column = columns[addJobColumn];
+    let company = document.getElementById("addCompany").value;
+    let position = document.getElementById("addPosition").value;
+    let startDate = document.getElementById("addStartDate").value;
+    let link = document.getElementById("addLink").value;
+    let description = document.getElementById("addDescription").value;
+    let applicationStatus = column.name;
+    let applicationDate = new Date().toDateString();
+    let id = uuidv4();
+    
+    if(company === "") setCompanyValid(true);
+    if(position === "") setPositionValid(true);
+    if(startDate === "") setStartDateValid(true);
+    if(link === "") setLinkValid(true);
+    if(description === "") setDescriptionValid(true);
+    
+    if (
+      company !== "" &&
+      position !== "" &&
+      startDate !== "" &&
+      link !== "" &&
+      description !== ""
+    ) {
+
+      column.items.push({
+        id: id,
+        companyName: company,
+        position: position,
+        link: link,
+        applicationDate: applicationDate,
+        applicationStatus: applicationStatus,
+        startDate: startDate,
+        description: description,
+      });
+      column.count++;
+      handleCloseAdd();
+    }
+  }
+
+  function deleteJob(){
+    let status = modalInfo.applicationStatus;
+    let column;
+    if(status === "Wish List" ) column = 1;
+    if(status === "Applied" ) column = 2;
+    if(status === "Interview" ) column = 3;
+    if(status === "Offer" ) column = 4;
+    if(status === "Regected" ) column = 5;
+    let theColumn = columns[column];
+
+    theColumn.items.splice(theColumn.items.findIndex(item => item.id === modalInfo.id), 1);
+    theColumn.count--;
+    handleClose();
+  }
+
+  function addJobModal(columnId) {
+    setAddJobColumn(columnId);
+    setShowAdd(true);
   }
 
   function openStats(item) {}
@@ -244,7 +317,20 @@ export default function TrackingPage() {
             {Object.entries(columns).map(([columnId, column], index) => {
               return (
                 <div className="Column">
-                  <h2 className="ColumnHeader">{column.name}</h2>
+                  <Row>
+                    <Col>
+                      <h2 className="ColumnHeader">{column.name}</h2>
+                    </Col>
+                    <Col>
+                      <h2
+                        onClick={() => addJobModal(columnId)}
+                        className="ColumnHeader"
+                        style={{ cursor: `pointer` }}
+                      >
+                        +
+                      </h2>
+                    </Col>
+                  </Row>
                   <h3 className="ColumnSubHeader">{column.count} Jobs</h3>
                   <Droppable droppableId={columnId} key={columnId}>
                     {(provided) => (
@@ -267,7 +353,7 @@ export default function TrackingPage() {
                                     <Row>
                                       <Col xs={2}>
                                         <img
-                                          style={{marginTop:`15%`}}
+                                          style={{ marginTop: `15%` }}
                                           className="JobCardImage"
                                           src="/Assets/facebookicon.png"
                                           alt=""
@@ -336,14 +422,97 @@ export default function TrackingPage() {
             {modalInfo.position}
           </Card.Title>
           <Card.Subtitle className="mb-2 text-muted">
-            { modalInfo.companyName}
+            {modalInfo.companyName}
           </Card.Subtitle>
-          <div style={{textAlign:`left`}}>
-            <Card.Text style={{margin:`2% 0`}}><span style={{fontWeight:`bold`}}>Start Date:</span> {modalInfo.startDate}</Card.Text>
-            <Card.Text style={{margin:`2% 0`}}><span style={{fontWeight:`bold`}}>Application Date:</span> {modalInfo.applicationDate}</Card.Text>
-            <Card.Text style={{margin:`2% 0`, overflowWrap:`anywhere`}}><span style={{fontWeight:`bold`}}>Job Description:</span> {modalInfo.description}</Card.Text>
+          <div style={{ textAlign: `left` }}>
+            <Card.Text style={{ margin: `2% 0` }}>
+              <span style={{ fontWeight: `bold` }}>Start Date:</span>{" "}
+              {modalInfo.startDate}
+            </Card.Text>
+            <Card.Text style={{ margin: `2% 0` }}>
+              <span style={{ fontWeight: `bold` }}>Application Date:</span>{" "}
+              {modalInfo.applicationDate}
+            </Card.Text>
+            <Card.Text style={{ margin: `2% 0`, overflowWrap: `anywhere` }}>
+              <span style={{ fontWeight: `bold` }}>Job Description:</span>{" "}
+              {modalInfo.description}
+            </Card.Text>
             <Card.Link href={modalInfo.link}>Posting</Card.Link>
           </div>
+          <Button onClick={deleteJob} className="DeleteCardButton">
+            Delete
+          </Button>
+        </Modal.Body>
+      </Modal>
+
+      <Modal centered show={showAdd} onHide={handleCloseAdd}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Job</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Card.Title style={{ textAlign: `start`, textOverflow: `ellipsis` }}>
+            <Row>
+              <Col xs={3}>Company</Col>
+              <Col xs={9}>
+                <Form.Control
+                  id="addCompany"
+                  type="text"
+                  placeholder="Google"
+                  isInvalid={addCompanyValid}
+                />
+              </Col>
+            </Row>
+          </Card.Title>
+          <Card.Title style={{ textAlign: `start`, textOverflow: `ellipsis` }}>
+            <Row>
+              <Col xs={3}>Position</Col>
+              <Col xs={9}>
+                <Form.Control id="addPosition" type="text" placeholder="SDE1" isInvalid={addPositionValid} />
+              </Col>
+            </Row>
+          </Card.Title>
+          <Card.Title style={{ textAlign: `start`, textOverflow: `ellipsis` }}>
+            <Row>
+              <Col xs={3}>Start Date</Col>
+              <Col xs={9}>
+                <Form.Control
+                  id="addStartDate"
+                  type="text"
+                  placeholder="May 2022"
+                  isInvalid={addStartDateValid}
+                />
+              </Col>
+            </Row>
+          </Card.Title>
+          <Card.Title style={{ textAlign: `start`, textOverflow: `ellipsis` }}>
+            <Row>
+              <Col xs={3}>Link</Col>
+              <Col xs={9}>
+                <Form.Control
+                  id="addLink"
+                  type="text"
+                  placeholder="www.google.com/careers"
+                  isInvalid={addLinkValid}
+                />
+              </Col>
+            </Row>
+          </Card.Title>
+          <Card.Title style={{ textAlign: `start`, textOverflow: `ellipsis` }}>
+            <Row>
+              <Col xs={3}>Description</Col>
+              <Col xs={9}>
+                <Form.Control
+                  id="addDescription"
+                  type="text"
+                  placeholder="Dream Job"
+                  isInvalid={addDescriptionValid}
+                />
+              </Col>
+            </Row>
+          </Card.Title>
+          <Button onClick={addJob} className="AddCardButton">
+            Add
+          </Button>
         </Modal.Body>
       </Modal>
     </Container>
