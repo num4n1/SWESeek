046a2f0813cbd9e35c0a3c07352256f0ad61aeee
Mohamed Numan <73037918+num4n1@users.noreply.github.com>
2021-12-16 04:25:44 -0700

    Update EndPointsSQLQueries.sql

--- SweSeek Database/EndPointsSQLQueries.sql
+++ SweSeek Database/EndPointsSQLQueries.sql
@@ -1,7 +1,7 @@
 Endpoint 3:
-SELECT U.Username,J.ListId, J.ListName,M.Serial, M.CompanyName, J.Position, J.Link, M.ApplicationDate, M.ApplicationStatus, J.StartDate, J.Description 
-FROM JOBS AS J, MYJOBS AS M, USERCREDENTIALS AS U, COMPANYCREDENTIALS AS C
-WHERE J.CompanyId = C.CompanyId AND M.CompanyId = C.CompanyId AND U.UserName = M.userName;
+SELECT U.Username,T.ListId, T.ListName,M.Id, M.CompanyName, J.Position, J.Link, M.ApplicationDate, M.ApplicationStatus, J.StartDate, J.Description 
+FROM JOBS AS J, MYJOBS AS M, USERCREDENTIALS AS U, COMPANYCREDENTIALS AS C, TRACKINGLIST AS T
+WHERE J.CompanyId = C.CompanyId AND M.CompanyId = C.CompanyId AND U.UserName = M.userName AND T.Id = M.Id; 
 
 Endpoint 4:
 SELECT Serial,companyName,position,link,applicationDate,applicationStatus,startDate,description FROM TRACKINGLIST;
@@ -18,3 +18,4 @@ Endpoint 9:
 SELECT J.companyId,position,size,industry,link,description FROM JOBS AS J, COMPANYCREDENTIALS AS C WHERE J.companyId = C.companyId;
 
 Endpoint 10:
+SELECT company,companySize,industry,role,totalComp FROM SALARY AS S, COMPANYCREDENTIALS AS C WHERE S.companyId = C.companyId;
