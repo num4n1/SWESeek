24946d535370e0b0336524f7c18054809deb055d
Nicholas Knapton <70191282+nickknapton12@users.noreply.github.com>
2021-12-17 16:53:13 -0700

    Update app.py

--- backendPython/app.py
+++ backendPython/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Whicket1'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'sweseek'
 app.config['SECRET_KEY'] = 'MySecretKey'
 
