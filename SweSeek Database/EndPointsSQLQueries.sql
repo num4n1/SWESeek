eca15bd105853ac74578c98fff0a13156b56852b
Mohamed Numan <73037918+num4n1@users.noreply.github.com>
2021-12-17 06:57:44 -0700

    Update EndPointsSQLQueries.sql

--- SweSeek Database/EndPointsSQLQueries.sql
+++ SweSeek Database/EndPointsSQLQueries.sql
@@ -111,6 +111,10 @@ SELECT value
 FROM QUESTIONTAGS
 WHERE tags=3;
 
+Endpoint 20.1:
+INSERT INTO MYQUESTIONRESOURCES(username,id)
+VALUES ('num4n', id);
+
 Endpoint 21.1: First get CompanyId using companyName, then use that CompanyId to add to JOBS along with the provided data.
 SELECT CompanyId FROM COMPANYCREDENTIALS WHERE companyName = 'Blizzard';
 INSERT INTO JOBS (CompanyId, company, position, StartDate, link, Description)
